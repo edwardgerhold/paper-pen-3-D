534211492a865b13bb7711c9e447c8763df3c892
edwardgerhold <edward.gerhold@googlemail.com>
2015-07-09 13:12:15 +0200

    full es5 implementation

---
+++ implement.js
@@ -0,0 +1,55 @@
+(function (exports) {
+
+function rad(deg) { 
+    return Math.PI/180*deg; 
+}
+
+var r_x = 1, r_y = 1, r_z = 1;
+
+var phi_x = rad(210), phi_y = rad(330), phi_z = rad(90);
+
+var xAxisCos = r_x * Math.cos(phi_x),
+    yAxisCos = r_y * Math.cos(phi_y),
+    zAxisCos = r_z * Math.cos(phi_z),
+    xAxisSin = r_x * Math.sin(phi_x),
+    yAxisSin = r_y * Math.sin(phi_y),
+    zAxisSin = r_z * Math.sin(phi_z);
+
+function transform2d(vec3) {
+    return [
+	x*xAxisCos + y*yAxisCos + z*zAxisCos,
+	x*xAxisSin + y*yAxisSin + z*zAxisSin
+    ];
+}
+
+function transform2dAll(avec3) {
+    return avec3.map(transform2d);
+}
+
+function settrans(op) {
+    if (op.phi_n) {
+	phi_x = op.phi_n[0];
+	phi_y = op.phi_n[1];
+	phi_z = op.phi_n[2];
+    }
+    if (op.r_n) {
+	r_x = op_r_n[0];
+	r_y = op_r_n[1];
+	r_z = op_r_n[2];
+    }
+}
+
+function gettrans() { 
+    return { 
+	phi_n: [phi_x, phi_y, phi_z], 
+	r_n: [r_x, r_y, r_z] 
+    }; 
+}
+
+exports.gettrans = gettrans;
+exports.settrans = settrans;
+exports.transform2d = transform2d;
+exports.transform2dAll = transform2dAll;
+exports.rad = rad;
+
+}(typeof exports != "undefined" ? exports : this));
